f02834b3d0fe1ad8d4018200357eb2643486338c
raizadaharoon <haroon.raizada@cloudreach.com>
2022-07-12 11:00:51 +0100

    commit

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/RaizadaHaroon/terraform-provider-launcher/provider"
-	_ "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: provider.Provider,
+		ProviderFunc: func() *schema.Provider {
+			return provider.Provider()
+		},
 	})
 }
